d895679eef71d312c97b9766e79e56671f41abe6
sohagmahin3 <sohagmahin3@gmail.com>
2019-03-17 18:10:12 +0600

    Added Profile update functionality

--- app/src/main/java/com/example/loginpanel/ProfileTab.java
+++ app/src/main/java/com/example/loginpanel/ProfileTab.java
@@ -6,6 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
@@ -13,6 +20,8 @@ import android.view.ViewGroup;
  */
 public class ProfileTab extends Fragment {
 
+    private EditText edtProfileName,edtProfileBio,edtProfileProfession,edtProfileHobbies,edtProfileSport;
+    private Button updateButton;
 
     public ProfileTab() {
         // Required empty public constructor
@@ -23,7 +32,46 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        edtProfileName = view.findViewById(R.id.edtProfileName);
+        edtProfileBio = view.findViewById(R.id.edtProfileBio);
+        edtProfileProfession = view.findViewById(R.id.edtProfileProfession);
+        edtProfileHobbies = view.findViewById(R.id.edtProfileHobbies);
+        edtProfileSport = view.findViewById(R.id.edtProfileSport);
+        updateButton = view.findViewById(R.id.updateButton);
+        final ParseUser currentUser  = ParseUser.getCurrentUser();
+
+        edtProfileName.setText(currentUser.get("ProfileName")+"");
+        edtProfileBio.setText(currentUser.get("ProfileBio")+"");
+        edtProfileProfession.setText(currentUser.get("ProfileProfession")+"");
+        edtProfileHobbies.setText(currentUser.get("ProfileHobbies")+"");
+        edtProfileSport.setText(currentUser.get("ProfileSport")+"");
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentUser.put("ProfileName",edtProfileName.getText().toString());
+                currentUser.put("ProfileBio",edtProfileBio.getText().toString());
+                currentUser.put("ProfileProfession",edtProfileProfession.getText().toString());
+                currentUser.put("ProfileHobbies",edtProfileHobbies.getText().toString());
+                currentUser.put("ProfileSport",edtProfileSport.getText().toString());
+
+                currentUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e==null){
+                            FancyToast.makeText(getContext(), "Info Updated! ", FancyToast.SUCCESS, FancyToast.LENGTH_LONG, true).show();
+                        }
+                        else {
+                            FancyToast.makeText(getContext(), e.getMessage(), FancyToast.ERROR, FancyToast.LENGTH_LONG, true).show();
+
+                        }
+                    }
+                });
+
+            }
+        });
+        return view;
     }
 
 }
