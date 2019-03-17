bf34370611ebf7558c8f5f18ab17d859910f8a77
sohagmahin3 <sohagmahin3@gmail.com>
2019-03-17 17:17:31 +0600

    Added Toolbar

--- app/src/main/java/com/example/loginpanel/SocialMediaActivity.java
+++ app/src/main/java/com/example/loginpanel/SocialMediaActivity.java
@@ -1,13 +1,33 @@
 package com.example.loginpanel;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class SocialMediaActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
 
+        setTitle("Social Media Activity");
+
+        toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager =findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager,false);
     }
 }
